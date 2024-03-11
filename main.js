14c9f4df43118468161b75f5441a22c6df28c28c
lucia <lucia.alfano07@gmail.com>
2024-03-11 18:22:24 -0300

    otra vez js

---
+++ main.js
@@ -0,0 +1,39 @@
+let nombre = prompt("Ingrese su nombre completo");
+let edad = prompt("Ingrese su edad")
+
+if (edad >= 18) {
+    alert("¡Bienvenido!")
+  
+} else
+    alert("Usted no posee permisos para ingresar");
+    console.log ("Menor de edad")
+
+let ingresoMensual = Number(prompt("Por favor, indique su ingreso mensual neto"));
+let ingresoAnual = Number(prompt("Por favor, indique sus ingresos anuales"));
+
+function sumar (ingresoMensual, ingresoAnual) {
+    return ingresoMensual + ingresoAnual;
+}
+
+let resultado = sumar;
+
+console.log(resultado)
+
+while(ingresoMensual >= 500000){
+    alert("Usted tiene una cuenta Gold!");
+    console.log("Usuario válido GOLD")
+    }
+
+while(ingresoAnual >= 800000){
+    alert("Usted tiene una cuenta Platinum!");
+    console.log("Usuario válido Platinum")
+    }
+
+if(ingresoMensual < 500000){
+    alert("Usted posee una cuenta basica");
+    console.log("Usuario valido BASICO")
+
+} else{
+    alert("Error")
+}
+    
