7acf29fa9120deff69636579ba0f5286f12f5919
lucia <lucia.alfano07@gmail.com>
2024-03-13 21:13:49 -0300

    nuevo codigo

--- main.js
+++ main.js
@@ -1,43 +1,24 @@
 let nombre = prompt("Ingrese su nombre completo");
-let edad = prompt("Ingrese su edad")
+let edad = prompt("Ingrese su edad");
 
 if (edad >= 18) {
-    alert("¡Bienvenido!")
-  
-} 
-while (edad<=18){
-    alert("Usted no posee permisos para ingresar");
-    console.log ("Menor de edad")
+    alert("¡Bienvenido!");
+} else {
+    do {
+        alert("Usted no posee permisos para ingresar");
+        console.log("Menor de edad");
+        edad = prompt("Ingrese su edad"); // Solicita nuevamente la edad hasta que sea mayor o igual a 18
+    } while (edad < 18);
 }
 
 let ingresoMensual = Number(prompt("Por favor, indique su ingreso mensual neto"));
-let ingresoAnual = Number(prompt("Por favor, indique sus ingresos anuales"));
-
-function sumar (ingresoMensual, ingresoAnual) {
-    return ingresoMensual + ingresoAnual;
-}
-
-let resultado = sumar;
-
-console.log(resultado)
-
-while(ingresoMensual >= 500000){
+if (ingresoMensual >= 500000) {
     alert("Usted tiene una cuenta Gold!");
-    console.log("Usuario válido GOLD")
-    break;
-    }
-
-while(ingresoAnual >= 800000){
+    console.log("Usuario válido GOLD");
+} else if (ingresoMensual >= 300000) {
     alert("Usted tiene una cuenta Platinum!");
-    console.log("Usuario válido Platinum")
-    break;
-    }
-
-if(ingresoMensual < 500000){
-    alert("Usted posee una cuenta basica");
-    console.log("Usuario valido BASICO")
-
-} else{
-    alert("Error")
+    console.log("Usuario válido Platinum");
+} else {
+    alert("Usted posee una cuenta básica");
+    console.log("Usuario válido BÁSICO");
 }
-    
